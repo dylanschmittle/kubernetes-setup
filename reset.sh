0eac6ef9be6a942ed2f0ff63e48e97a80657051a
Dylan <da.schmittle89@gmail.com>
2020-07-18 20:57:46 -0400

    changed reset script to save old manifests

--- reset.sh
+++ reset.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo "Deleting Operator Manifests"
-rm mattermost-operator.yaml
-rm minio-operator.yaml
-rm mysql-operator.yaml
+echo "Archiving Manifests"
+TIME=$(date +%s)
+mkdir old-$(TIME)
+mv *-operator.yaml old-$(TIME)/
+cp mattermost.yaml old-$(TIME)/mattermost.yaml.old
 echo "Resetting Domain to mm.example.com in mattermost.yaml"
 OLD_DOMAIN=$(cat setup.sh | grep "DOMAIN_NAME" | cut -d "\"" -f2)
 sed -i "s/$OLD_DOMAIN/mm.example.com/g" mattermost.yaml

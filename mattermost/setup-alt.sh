64450a8c3898799869cb41ef8fff5321cfb1906e
Dylan <da.schmittle89@gmail.com>
2020-07-19 04:57:21 -0400

    moved to other install script

--- mattermost/setup-alt.sh
+++ mattermost/setup-alt.sh
@@ -9,7 +9,6 @@ kubectl create ns mattermost-operator
 kubectl apply -n mattermost-operator -f https://raw.githubusercontent.com/mattermost/mattermost-operator/master/docs/mattermost-operator/mattermost-operator.yaml
 kubectl create ns mattermost
 cat mattermost-template.yaml > mattermost-nginx.yaml
-sed -i "s/mm.example.com/mm.92c62c91-7a4e-4dfb-8e6e-a062063a70ae.nodes.k8s.fr-par.scw.cloud/g" mattermost-nginx.yaml
+sed -i "s/mm.example.com/mattermost.dylanschmittle.casa/g" mattermost-nginx.yaml
 sed -i "s/traefik/nginx/g" mattermost-nginx.yaml
 kubectl apply -n mattermost -f mattermost-nginx.yaml
-cd ../cert-manager && sh setup.sh

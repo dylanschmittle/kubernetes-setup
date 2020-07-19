aff270b3d6965746e4ae7ef3c1c0f2fbfc6328eb
Dylan <da.schmittle89@gmail.com>
2020-07-19 08:06:24 -0400

    added other compose files

--- jitsi/env-gen.sh
+++ jitsi/env-gen.sh
@@ -4,5 +4,5 @@ cd docker-jitsi-meet
 cat env.example > .env
 sh gen-passwords.sh
 echo "set .env file values as needed"
-echo "replace port envs in compose file with actual numbers from .env"
+echo "replace port envs in all 3 compose files with actual numbers from .env"
 echo "then run convert.sh"
